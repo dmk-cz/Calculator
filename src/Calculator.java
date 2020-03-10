cc6cc686fc2dd11da309c9c708d1dfc15b9d2628
Dmk <57073731+dmk-cz@users.noreply.github.com>
2020-03-10 22:30:31 +0200

    version 1.3 with Rome numerals

--- src/Calculator.java
+++ src/Calculator.java
@@ -16,57 +16,332 @@ public class Calculator {
         boolean gotMultiply = userStr.contains("*");
         boolean gotDivision = userStr.contains("/");
 
-        if(gotPlus == true) {
+
+        if (gotPlus == true) {
             int index = userStr.indexOf("+");
             //number 1
-            String s = userStr.substring(0,index);
-            a = Integer.parseInt(s);
+            String s = userStr.substring(0, index);
+
+            switch (s) {
+                case("I"):
+                    a = 1;
+                    break;
+                case ("II"):
+                    a = 2;
+                    break;
+                case ("III"):
+                    a = 3;
+                    break;
+                case ("IV"):
+                    a = 4;
+                    break;
+                case ("V"):
+                    a = 5;
+                    break;
+                case ("VI"):
+                    a = 6;
+                    break;
+                case ("VII"):
+                    a = 7;
+                    break;
+                case ("VIII"):
+                    a = 8;
+                    break;
+                case ("IX"):
+                    a = 9;
+                    break;
+                case ("X"):
+                    a = 10;
+                    break;
+                default:
+                    a = Integer.parseInt(s);
+            }
+
             //number 2
-            String s2 = userStr.substring(index+1, len);
-            b = Integer.parseInt(s2);
+            String s2 = userStr.substring(index + 1, len);
+
+            switch (s2) {
+                case("I"):
+                    b = 1;
+                    break;
+                case ("II"):
+                    b = 2;
+                    break;
+                case ("III"):
+                    b = 3;
+                    break;
+                case ("IV"):
+                    b = 4;
+                    break;
+                case ("V"):
+                    b = 5;
+                    break;
+                case ("VI"):
+                    b = 6;
+                    break;
+                case ("VII"):
+                    b = 7;
+                    break;
+                case ("VIII"):
+                    b = 8;
+                    break;
+                case ("IX"):
+                    b = 9;
+                    break;
+                case ("X"):
+                    b = 10;
+                    break;
+                default:
+                    b = Integer.parseInt(s2);
+            }
+            //b = Integer.parseInt(s2);
 
             result = a + b;
         }
 
-        if(gotMinus == true) {
+        if (gotMinus == true) {
             int index = userStr.indexOf("-");
             //number 1
-            String s = userStr.substring(0,index);
-            a = Integer.parseInt(s);
+            String s = userStr.substring(0, index);
+
+            switch (s) {
+                case("I"):
+                    a = 1;
+                    break;
+                case ("II"):
+                    a = 2;
+                    break;
+                case ("III"):
+                    a = 3;
+                    break;
+                case ("IV"):
+                    a = 4;
+                    break;
+                case ("V"):
+                    a = 5;
+                    break;
+                case ("VI"):
+                    a = 6;
+                    break;
+                case ("VII"):
+                    a = 7;
+                    break;
+                case ("VIII"):
+                    a = 8;
+                    break;
+                case ("IX"):
+                    a = 9;
+                    break;
+                case ("X"):
+                    a = 10;
+                    break;
+                default:
+                    a = Integer.parseInt(s);
+            }
             //number 2
-            String s2 = userStr.substring(index+1, len);
-            b = Integer.parseInt(s2);
+            String s2 = userStr.substring(index + 1, len);
+
+            switch (s2) {
+                case("I"):
+                    b = 1;
+                    break;
+                case ("II"):
+                    b = 2;
+                    break;
+                case ("III"):
+                    b = 3;
+                    break;
+                case ("IV"):
+                    b = 4;
+                    break;
+                case ("V"):
+                    b = 5;
+                    break;
+                case ("VI"):
+                    b = 6;
+                    break;
+                case ("VII"):
+                    b = 7;
+                    break;
+                case ("VIII"):
+                    b = 8;
+                    break;
+                case ("IX"):
+                    b = 9;
+                    break;
+                case ("X"):
+                    b = 10;
+                    break;
+                default:
+                    b = Integer.parseInt(s2);
+            }
 
             result = a - b;
         }
 
-        if(gotMultiply == true) {
+        if (gotMultiply == true) {
             int index = userStr.indexOf("*");
             //number 1
-            String s = userStr.substring(0,index);
-            a = Integer.parseInt(s);
+            String s = userStr.substring(0, index);
+
+            switch (s) {
+                case("I"):
+                    a = 1;
+                    break;
+                case ("II"):
+                    a = 2;
+                    break;
+                case ("III"):
+                    a = 3;
+                    break;
+                case ("IV"):
+                    a = 4;
+                    break;
+                case ("V"):
+                    a = 5;
+                    break;
+                case ("VI"):
+                    a = 6;
+                    break;
+                case ("VII"):
+                    a = 7;
+                    break;
+                case ("VIII"):
+                    a = 8;
+                    break;
+                case ("IX"):
+                    a = 9;
+                    break;
+                case ("X"):
+                    a = 10;
+                    break;
+                default:
+                    a = Integer.parseInt(s);
+            }
+
             //number 2
-            String s2 = userStr.substring(index+1, len);
-            b = Integer.parseInt(s2);
+            String s2 = userStr.substring(index + 1, len);
+
+            switch (s2) {
+                case("I"):
+                    b = 1;
+                    break;
+                case ("II"):
+                    b = 2;
+                    break;
+                case ("III"):
+                    b = 3;
+                    break;
+                case ("IV"):
+                    b = 4;
+                    break;
+                case ("V"):
+                    b = 5;
+                    break;
+                case ("VI"):
+                    b = 6;
+                    break;
+                case ("VII"):
+                    b = 7;
+                    break;
+                case ("VIII"):
+                    b = 8;
+                    break;
+                case ("IX"):
+                    b = 9;
+                    break;
+                case ("X"):
+                    b = 10;
+                    break;
+                default:
+                    b = Integer.parseInt(s2);
+            }
 
             result = a * b;
         }
 
-        if(gotDivision == true) {
+        if (gotDivision == true) {
             int index = userStr.indexOf("/");
             //number 1
-            String s = userStr.substring(0,index);
-            a = Integer.parseInt(s);
+            String s = userStr.substring(0, index);
+
+            switch (s) {
+                case("I"):
+                    a = 1;
+                    break;
+                case ("II"):
+                    a = 2;
+                    break;
+                case ("III"):
+                    a = 3;
+                    break;
+                case ("IV"):
+                    a = 4;
+                    break;
+                case ("V"):
+                    a = 5;
+                    break;
+                case ("VI"):
+                    a = 6;
+                    break;
+                case ("VII"):
+                    a = 7;
+                    break;
+                case ("VIII"):
+                    a = 8;
+                    break;
+                case ("IX"):
+                    a = 9;
+                    break;
+                case ("X"):
+                    a = 10;
+                    break;
+                default:
+                    a = Integer.parseInt(s);
+            }
+
             //number 2
-            String s2 = userStr.substring(index+1, len);
-            b = Integer.parseInt(s2);
+            String s2 = userStr.substring(index + 1, len);
+
+            switch (s2) {
+                case("I"):
+                    b = 1;
+                    break;
+                case ("II"):
+                    b = 2;
+                    break;
+                case ("III"):
+                    b = 3;
+                    break;
+                case ("IV"):
+                    b = 4;
+                    break;
+                case ("V"):
+                    b = 5;
+                    break;
+                case ("VI"):
+                    b = 6;
+                    break;
+                case ("VII"):
+                    b = 7;
+                    break;
+                case ("VIII"):
+                    b = 8;
+                    break;
+                case ("IX"):
+                    b = 9;
+                    break;
+                case ("X"):
+                    b = 10;
+                    break;
+                default:
+                    b = Integer.parseInt(s2);
+            }
 
             result = a / b;
         }
 
 
-
         System.out.println(result);
-
     }
 }
--- src/CalculatorLauncher.java
+++ src/CalculatorLauncher.java
@@ -1,7 +1,7 @@
 public class CalculatorLauncher {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("CALCULATOR v 1.2");
+        System.out.println("CALCULATOR v 1.3");
         System.out.println("*****************");
         System.out.println("Program calculator. Enter numbers from 0 to 9 inclusive");
         System.out.println("Enter user inputs using following format:");
@@ -9,6 +9,9 @@ public class CalculatorLauncher {
         System.out.println("2/4");
         System.out.println("3*6");
         System.out.println("1-1");
+        System.out.println("There is also a format for Roman numerals:");
+        System.out.println("1-I, 2-II, 3-III, 4-IV, 5-V, 6-VI, 7-VII, " +
+                "8-VIII, 9-IX, 10-X");
         System.out.println("Enter a string without spaces. Then press enter");
         System.out.println("---------------------");
         Calculator calculator = new Calculator();
